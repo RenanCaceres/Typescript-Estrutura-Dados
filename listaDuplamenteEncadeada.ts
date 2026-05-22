523c2cfc34d7e4060f7983b3ccafa1e406eaa9d8
Caelayne Aparecida Ribeiro Soares <soarescaelayne14@gmail.com>
2026-05-22 19:54:20 -0300

    Funções busca_elemento, edita_elemento, remove_incio, remove_final e remove_mei

--- listaDuplamenteEncadeada.ts
+++ listaDuplamenteEncadeada.ts
@@ -75,6 +75,92 @@ class ListaDuplamenteEncadeada<T> {
             atual = atual.proximo;
         }
     }
+    
+    buscaElemento(dados: T): Node<T> | null{
+         let atual = this.inicio;
+ 
+    while (atual !== null) {
+        if (atual.dados === valor) {
+            return atual;
+        }
+        atual = atual.proximo; 
+    }
+ 
+    return null;
+    }
+
+edita_elemento(valorAntigo: T, novoValor: T): boolean {
+    const no = this.busca_elemento(valorAntigo);
+ 
+    if (no === null) return false;
+ 
+    no.dados = novoValor;
+    return true;
+}
+ 
+remove_inicio(): T | null {
+    if (this.inicio === null) return null;
+ 
+    const removido = this.inicio;
+ 
+    if (this.inicio === this.fim) {
+        this.inicio = null; 
+        this.fim = null;
+        return removido.dados;
+    }
+ 
+    this.inicio = this.inicio.proximo;
+ 
+    if (this.inicio !== null) {
+        this.inicio.anterior = null;
+    }
+ 
+    return removido.dados;
+}
+ 
+remove_final(): T | null {
+    if (this.fim === null) return null;
+ 
+    const removido = this.fim;
+ 
+    if (this.inicio === this.fim){
+        this.inicio = null;
+        this.fim = null;
+        return removido.dados;
+    }
+ 
+    this.fim = this.fim.anterior;
+ 
+    if (this.fim !== null) { 
+        this.fim.proximo = null;
+    }
+ 
+    return removido.dados;
+}
+ 
+remove_meio(valor: T): boolean {
+    const no = this.busca_elemento(valor);
+ 
+    if (no === null) return false;
+ 
+    if (no === this.inicio) {
+        this.remove_inicio();
+        return true;
+    }
+ 
+    if (no === this.fim) {
+        this.remove_final();
+        return true;
+    }
+ 
+    const anterior = no.anterior;
+    const proximo =  no.proximo;
+    
+    if (anterior) anterior.proximo = proximo;
+    if (proximo)  proximo.anterior = anterior;
+ 
+    return true;
+}
 }
 
 export default ListaDuplamenteEncadeada;
